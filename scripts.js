5c27ed38d2616f082ee9678894a8ed69b9ed0653
Omar Maskati <70995663+omaskati@users.noreply.github.com>
2023-11-21 00:46:53 -0500

    Make all buttons update display

--- scripts.js
+++ scripts.js
@@ -15,8 +15,7 @@ const divide = function (a,b){
     return a / b;
 }
 
-let operator = "";
-let a = b = 0;
+let operator = a = b = "";
 
 const operate = function(a, operator, b) {
     switch (operator){
@@ -26,7 +25,7 @@ const operate = function(a, operator, b) {
         case "-":
             return subtract(a,b);
         
-        case "*":
+        case "x":
             return multiply(a,b);
         
         case "/":
@@ -35,4 +34,34 @@ const operate = function(a, operator, b) {
         default:
             return NaN;
     }
+}
+
+const display = document.querySelector("#display");
+
+const clearDisplay = function(){
+    display.textContent="";
+    operator = a = b = "";
+}
+
+const btnClear = document.querySelector('#clear');
+btnClear.onclick = clearDisplay;
+
+const btnEquals = document.querySelector("#equals");
+btnEquals.onclick = function(){
+    let result = operate(+a, operator, +b);
+    display.textContent = result;
+};
+
+const digitButtons = document.querySelectorAll("button.digit-btn");
+for (let i=0; i< digitButtons.length; i++) {
+    digitButtons[i].onclick = function (){
+        display.textContent += digitButtons[i].innerText;
+    };
+}
+
+const opButtons = document.querySelectorAll("button.operator-btn");
+for (let i=0; i< opButtons.length; i++) {
+    opButtons[i].onclick = function (){
+        display.textContent += " " + opButtons[i].innerText + " ";
+    };
 }
