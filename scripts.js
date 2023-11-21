4776e157ba2d55e74aa0366a18fa15eef456e04b
Omar Maskati <70995663+omaskati@users.noreply.github.com>
2023-11-21 01:47:09 -0500

    Make all 2-digit calculations work properly

--- scripts.js
+++ scripts.js
@@ -16,6 +16,12 @@ const divide = function (a,b){
 }
 
 let operator = a = b = "";
+let aIsActive = true;
+let equalsPressed = false;
+
+const switchAB = function(){
+    aIsActive = !aIsActive;
+}
 
 const operate = function(a, operator, b) {
     switch (operator){
@@ -41,20 +47,39 @@ const display = document.querySelector("#display");
 const clearDisplay = function(){
     display.textContent="";
     operator = a = b = "";
+    aIsActive = true;
+    equalsPressed=false;
 }
 
 const btnClear = document.querySelector('#clear');
 btnClear.onclick = clearDisplay;
 
 const btnEquals = document.querySelector("#equals");
-btnEquals.onclick = function(){
-    let result = operate(+a, operator, +b);
-    display.textContent = result;
+const operateAndUpdate = function(){
+    if(!aIsActive && b !== ""){
+        console.log(`trying ${a} ${operator} ${b} `);
+        let result = operate(+a, operator, +b);
+        display.textContent = result;
+        a = result;
+        b = operator = "";
+    }
+    switchAB();
 };
+btnEquals.onclick = function() {
+    operateAndUpdate();
+    equalsPressed = true;
+};
+
 
 const digitButtons = document.querySelectorAll("button.digit-btn");
 for (let i=0; i< digitButtons.length; i++) {
     digitButtons[i].onclick = function (){
+        if(aIsActive){
+            if(equalsPressed) {clearDisplay();}//reset everything
+            
+            a += digitButtons[i].innerText;
+        }
+        else b += digitButtons[i].innerText;
         display.textContent += digitButtons[i].innerText;
     };
 }
@@ -62,6 +87,9 @@ for (let i=0; i< digitButtons.length; i++) {
 const opButtons = document.querySelectorAll("button.operator-btn");
 for (let i=0; i< opButtons.length; i++) {
     opButtons[i].onclick = function (){
-        display.textContent += " " + opButtons[i].innerText + " ";
+        if(operator !== "") return;
+        operator = opButtons[i].innerText;
+        operateAndUpdate(); //won't do anything if aIsActive
+        display.textContent += " " + operator + " ";
     };
 }
